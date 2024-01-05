2da473422bc3bc9d61ca74c09b436bb8a0e22916
Sharath <Sharath@LAPTOP-N4VGTCJD>
2024-01-05 22:44:06 +0530

    Kafka producer project

---
+++ Kafka-producer-exampleproj/src/main/java/com/example/demo/config/KafkaProducerConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+	
+	@Bean
+	public NewTopic createTopic() {
+		return new NewTopic("MykafkaTopic1", 3, (short)1);
+	}
+
+}
